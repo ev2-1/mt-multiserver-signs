67541f5862dc327b1fbb045455e56e658a7b7cb7
Edzell <derz@elidragon.com>
2022-05-26 18:16:51 +0200

    mt-multiserver-signs

--- go.mod
+++ go.mod
@@ -1,12 +1,12 @@
-module github.com/HimbeerserverDE/mt-multiserver-chatcommands
+module github.com/ev2-1/mt-multiserver-signs
 
 go 1.18
 
-require github.com/HimbeerserverDE/mt-multiserver-proxy v0.0.0-20220514170657-54c9f9bb8d42
-
 require (
-	github.com/HimbeerserverDE/srp v0.0.0 // indirect
-	github.com/anon55555/mt v0.0.0-20210919124550-bcc58cb3048f // indirect
+	github.com/HimbeerserverDE/mt-multiserver-proxy v0.0.0-20220514170657-54c9f9bb8d42
+	github.com/anon55555/mt v0.0.0-20210919124550-bcc58cb3048f
 )
 
-replace github.com/HimbeerserverDE/mt-multiserver-proxy v0.0.0-20220514170657-54c9f9bb8d42 => ../../proxy
+require github.com/HimbeerserverDE/srp v0.0.0 // indirect
+
+replace github.com/HimbeerserverDE/mt-multiserver-proxy => ../../proxy
--- sign.go
+++ sign.go
@@ -1,87 +1,109 @@
-package main
+package signs
 
 import (
-	"fmt"
-	"strings"
 	"bufio"
+	"fmt"
+	"github.com/anon55555/mt"
+	"net/http"
 	"os"
+	"strings"
 	"sync"
-	"strconv"
+	"unicode/utf8"
 )
 
-type charMapE struct {
-	encoded string
-	width   int64
-}
-
-var charMap   map[rune]*charMapE
+var charMap map[rune]string
 var charMapMu sync.Once
 
 const (
-	SIGN_WIDTH = 115
-	LINE_LENGTH = 15
-	NUMBER_OF_LINES = 4
-	LINE_HEIGHT = 14
-	CHAR_WIDTH = 5
+	SIGN_WIDTH         = 115
+	LINE_LENGTH        = 15
+	NUMBER_OF_LINES    = 4
+	LINE_HEIGHT        = 14
+	CHAR_WIDTH         = 5
 	PRINTED_CHAR_WIDTH = CHAR_WIDTH + 1
+
+	SignUrl = "https://git.minetest.land/MineClone2/MineClone2/raw/commit/d887a9731055fb041624cb6a1a09fa4ee7365bf6/mods/ITEMS/mcl_signs/characters.txt"
 )
 
-func loadCharMap() {
+// LoadCharMap downloads the character map from constant SignUrl (if not already have)
+func LoadCharMap() {
 	charMapMu.Do(func() {
-		f, err := os.Open("characters.txt")
+		charMap = make(map[rune]string)
+
+		resp, err := http.Get(SignUrl)
 		if err != nil {
-			fmt.Println("[MT_SIGNS] Cant read characters.txt file!")
+			fmt.Println("[SIGNS] couldn't download sign from", SignUrl)
 			os.Exit(-1)
 		}
 
-		s := bufio.NewScanner(f)
-		var state uint8 = 0
+		s := bufio.NewScanner(resp.Body)
+		var state uint8 = 1
 		var char string
 		var eChar string
-		
 		for s.Scan() {
 			switch state {
-			case 0: // every third line
-				char = s.Text()
 			case 1:
-				eChar = s.Text()
+				char = s.Text()
 			case 2:
-				w, _ := strconv.ParseInt(s.Text(), 10, 64)
-			
-				charMap[rune(char[0])] = &charMapE{
-					encoded: eChar,
-					width: w,
-				}
-
+				eChar = s.Text()
+			case 3:
+				ru, _ := utf8.DecodeRuneInString(char)
+				charMap[ru] = "micl2_" + eChar
 				state = 0
 			}
-		
+
 			state++
 		}
 	})
 }
 
-func generateTexture(text string) string {
-	texture := fmt.Sprintf("[combine:%nx%n", SIGN_WIDTH, SIGN_WIDTH)
+// TestSign generates a test sign (wow)
+func TestSign(wall bool) mt.Texture {
+	return GenerateSignTexture(
+		"O"+CenterLine("", '-', 14)+"O\n|"+CenterLine("test", ' ', 14)+"|\n|"+CenterLine("odd", ' ', 14)+"|\nO--------------O",
+		wall,
+		"black",
+	)
+}
 
-	// TODO: "mcl_signs:wall_sign" starts at ypos = 30 else 0
+// center line centers a line, by padding the same left and right (right one more with odd length strings)
+func CenterLine(line string, filler rune, width int) string {
+	if len(line) > width {
+		return line
+	}
 
+	// calculate padding
+	width = (width - len(line)) / 2
+
+	if len(line)%2 == 0 { // if even
+		return strings.Repeat(string(filler), width) + line + strings.Repeat(string(filler), width)
+	} else { // not even
+		return strings.Repeat(string(filler), width) + line + strings.Repeat(string(filler), width+1)
+	}
+}
+
+// GenerateSignTexture generates a sign texture
+func GenerateSignTexture(text string, wall bool, color string) mt.Texture {
+	LoadCharMap()
+
+	texture := fmt.Sprintf("[combine:%dx%d", SIGN_WIDTH, SIGN_WIDTH)
 	ypos := 0
+	if wall {
+		ypos = 30
+	}
 
 	for _, line := range strings.Split(text, "\n") {
-		xpos := 0
-	
+		xpos := 10
 		for _, letter := range line {
-			if charMap[letter] != nil {
-				// ":"..xpos..","..ypos.."="..parsed[i]..".png"
-				texture += fmt.Sprintf(":%n,%n=%s.png", xpos, ypos, charMap[letter].encoded)
-
-				xpos += PRINTED_CHAR_WIDTH
+			if charMap[letter] != "" {
+				texture += fmt.Sprintf(":%d,%d=%s.png", xpos, ypos, charMap[letter])
 			}
+
+			xpos += PRINTED_CHAR_WIDTH
 		}
 
 		ypos += LINE_HEIGHT
 	}
 
-	return texture
+	return mt.Texture(texture + "^[colorize:" + color + ":128")
 }
